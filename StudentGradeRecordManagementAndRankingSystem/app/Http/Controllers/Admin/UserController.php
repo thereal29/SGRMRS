d9bdb0c9fd27892490c1b157b8ea2cbda0c64c98
DARYL ACAMPADO PIAMONTE <63008207+thereal29@users.noreply.github.com>
2023-06-08 22:51:47 +0800

    first commit

--- StudentGradeRecordManagementAndRankingSystem/app/Http/Controllers/Admin/UserController.php
+++ StudentGradeRecordManagementAndRankingSystem/app/Http/Controllers/Admin/UserController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function maincontent(){
-        return view('admin.users_list', ["msg"=>"This is users page"]);
+        return view('admin.users_list', ["mssg"=>"This is users page"]);
     }
 }
